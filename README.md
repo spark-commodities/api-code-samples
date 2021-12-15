b26d2dac3921a8b18a7079c72f2d714875129600
Charles Vallantin Dulac <charles.vallantindulac@sparkcommodities.com>
2021-12-15 10:48:33 +0800

    Update python sample

--- README.md
+++ README.md
@@ -4,3 +4,59 @@
 * Java 8: [readme](java8)
 
 More information here: https://developers.sparkcommodities.com/
+
+**An OAuth2 client is required.**
+
+Please refer to https://app.sparkcommodities.com/freight/data-integrations/api if you 
+don't have any.
+
+
+## Running the Python sample:
+
+Python >= 3.1 is required.  You can either execute the python script:
+
+- By providing the `client_credentials.csv` file path
+
+```shell
+$ python spark_api_example.py <client_credentials_csv_file_path>
+```
+
+- By providing 2 environment variables:
+
+```shell
+$ export SPARK_CLIENT_ID=XXXX
+$ export CLIENT_SECRET=YYYY
+$ python spark_api_example.py
+```
+
+An execution example:
+
+```shell
+$ python spark_api_example.py ~/Downloads/client_credentials.csv
+```
+
+Output:
+
+```
+>>>> Running Spark API Python sample...
+>>>> Found credentials!
+>>>> Client_id=166f3ee3-cfdc-46cc-ad43-3c1894697bb4, client_secret=2a14d****
+>>>> Successfully fetched an access token eyJ0e****, valid 1799 seconds.
+>>>> Contracts:
+Spark25S Pacific 160 TFDE
+Spark25F Pacific 160 TFDE
+Spark25Fo Pacific 160 TFDE
+...
+>>>> Get latest price release for spark25s
+release date = 2020-12-14
+Spark Price is USD 186000 /day for period starting on 2020-12-29
+>>>> Get latest price release for spark25f
+release date = 2020-12-14
+Spark Price is USD 187500 /day for period starting on 2020-01-01
+>>>> Get latest price release for spark25fo
+release date = 2020-12-14
+Spark Price is USD 192250 /day for period starting on 2021-01-01
+Spark Price is USD 128000 /day for period starting on 2021-02-01
+...
+>>>> Done
+```
--- spark_api_example.py
+++ spark_api_example.py
@@ -4,36 +4,131 @@
 Spark API Python example.
 
 This script retrieves an access token then fetches available Spark contracts.
+It requires Python 3 (version >=3.1)
 
 Usage:
 
-$ python spark_api_example.py
+- By providing the client_credentials file path
 
-Requires Python 3 (version >=3.1)
+$ python spark_api_example.py <client_credentials_csv_file_path>
+
+- By providing 2 environment variables:
+
+$ export SPARK_CLIENT_ID=XXXX
+$ export CLIENT_SECRET=YYYY
+$ python spark_api_example.py
 """
 
 import json
 import os
+import sys
 from base64 import b64encode
+from urllib.parse import urljoin
+
 
 try:
     from urllib import request, parse
+    from urllib.error import HTTPError
 except ImportError:
     raise RuntimeError("Python 3 required")
-
-
-# Your credentials. You can edit this file or set environment variables.
-# Please send an email to info@sparkcommodities.com to request credentials.
-CLIENT_ID = os.getenv("SPARK_CLIENT_ID")
-CLIENT_SECRET = os.getenv("SPARK_CLIENT_SECRET")
-if not CLIENT_ID or not CLIENT_SECRET:
-    raise RuntimeError("SPARK_CLIENT_ID and SPARK_CLIENT_SECRET env vars required")
 
 
 API_BASE_URL = "https://api.sparkcommodities.com"
 
 
-def get_access_token():
+def retrieve_credentials(file_path=None):
+    """
+    Find credentials either by reading the client_credentials file or reading
+    environment variables
+    """
+    if file_path is None:
+
+        client_id = os.getenv("SPARK_CLIENT_ID")
+        client_secret = os.getenv("SPARK_CLIENT_SECRET")
+        if not client_id or not client_secret:
+            raise RuntimeError(
+                "SPARK_CLIENT_ID and SPARK_CLIENT_SECRET environment vars required"
+            )
+    else:
+        # Parse the file
+        if not os.path.isfile(file_path):
+            raise RuntimeError("The file {} doesn't exist".format(file_path))
+
+        with open(file_path) as fp:
+            first_line = fp.readline()
+            if "clientId,clientSecret" not in first_line:
+                print("First line: {}".format(first_line))
+                raise RuntimeError(
+                    "The specified file {} doesn't look like to be a Spark API client "
+                    "credentials file".format(file_path)
+                )
+
+            second_line = fp.readline()
+
+        client_id, client_secret = second_line.split(",")
+
+    print(">>>> Found credentials!")
+    print(
+        ">>>> Client_id={}, client_secret={}****".format(client_id, client_secret[:5])
+    )
+
+    return client_id, client_secret
+
+
+def do_api_post_query(uri, body, headers):
+    url = urljoin(API_BASE_URL, uri)
+
+    data = json.dumps(body).encode("utf-8")
+
+    # HTTP POST request
+    req = request.Request(url, data=data, headers=headers)
+    try:
+        response = request.urlopen(req)
+    except HTTPError as e:
+        print("HTTP Error: ", e.code)
+        print(e.read())
+        sys.exit(1)
+
+    resp_content = response.read()
+
+    # The server must return HTTP 201. Raise an error if this is not the case
+    assert response.status == 201, resp_content
+
+    # The server returned a JSON response
+    content = json.loads(resp_content)
+
+    return content
+
+
+def do_api_get_query(uri, access_token):
+    url = urljoin(API_BASE_URL, uri)
+
+    headers = {
+        "Authorization": "Bearer {}".format(access_token),
+        "Accept": "application/json",
+    }
+
+    # HTTP POST request
+    req = request.Request(url, headers=headers)
+    try:
+        response = request.urlopen(req)
+    except HTTPError as e:
+        print("HTTP Error: ", e.code)
+        print(e.read())
+        sys.exit(1)
+
+    resp_content = response.read()
+
+    # The server must return HTTP 201. Raise an error if this is not the case
+    assert response.status == 200, resp_content
+
+    # The server returned a JSON response
+    content = json.loads(resp_content)
+
+    return content
+
+
+def get_access_token(client_id, client_secret):
     """
     Get a new access_token. Access tokens are the thing that applications use to make
     API requests. Access tokens must be kept confidential in storage.
@@ -49,73 +144,105 @@ def get_access_token():
     # Note: for the sake of this example, we choose to use the Python urllib from the
     # standard lib. One should consider using https://requests.readthedocs.io/
 
-    url = "{}/oauth/token/".format(API_BASE_URL)
-
-    payload = "{}:{}".format(CLIENT_ID, CLIENT_SECRET).encode()
+    payload = "{}:{}".format(client_id, client_secret).encode()
     headers = {
         "Authorization": b64encode(payload).decode(),
         "Accept": "application/json",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+    }
+    body = {
+        "grantType": "clientCredentials",
+        "scopes": "read:lng-freight-prices",
     }
 
-    body = {"grantType": "clientCredentials", "scopes": "read:lng-freight-prices"}
-    data = json.dumps(body).encode("utf-8")
-
-    # POST request (data is not None)
-    req = request.Request(url, data=data, headers=headers)
-    response = request.urlopen(req)
-    resp_content = response.read()
-
-    # The server must return a HTTP 201. Raise an error if this is not the case
-    assert response.status == 201, response.status
-
-    # The server returned a JSON response
-    content = json.loads(resp_content)
+    content = do_api_post_query(uri="/oauth/token/", body=body, headers=headers)
 
     print(
-        "Successfully fetched an access token {}****, valid {} seconds.".format(
-            content["accessToken"][:10], content["expiresIn"]
+        ">>>> Successfully fetched an access token {}****, valid {} seconds.".format(
+            content["accessToken"][:5], content["expiresIn"]
         )
     )
 
     return content["accessToken"]
 
 
+#
+# Spark data fetching functions:
+#
+
+
 def list_contracts(access_token):
     """
-    Fetch available contracts.
+    Fetch available contracts. Return contract ticker symbols
 
     # Procedure:
 
-    Do a GET query with a Bearer token authorization HTTP header.
+    Do a GET query to /v1.0/contracts/ with a Bearer token authorization HTTP header.
     """
+    content = do_api_get_query(uri="/v1.0/contracts/", access_token=access_token)
 
-    url = "{}/v1.0/contracts/".format(API_BASE_URL)
-
-    headers = {
-        "Authorization": "Bearer {}".format(access_token),
-        "Accept": "application/json",
-    }
-
-    req = request.Request(url, headers=headers)
-    response = request.urlopen(req)
-    content = response.read()
-
-    # The server must return a HTTP 200. Raise an error if this is not the case
-    assert response.status == 200, content
-
-    # The server returned a JSON response
-    data = json.loads(content)
-
-    print("Contracts:")
-    for contract in data["data"]:
+    print(">>>> Contracts:")
+    tickers = []
+    for contract in content["data"]:
         print(contract["fullName"])
+        tickers.append(contract["id"])
+
+    return tickers
 
 
-def main():
-    access_token = get_access_token()
-    list_contracts(access_token)
+def get_latest_price_releases(access_token, ticker):
+    """
+    For each contract, fetch then display the latest price release
+
+    # Procedure:
+
+    Do GET queries to /v1.0/contracts/{contract_ticker_symbol}/price-releases/latest/
+    with a Bearer token authorization HTTP header.
+    """
+    content = do_api_get_query(
+        uri="/v1.0/contracts/{}/price-releases/latest/".format(ticker),
+        access_token=access_token,
+    )
+
+    release_date = content["data"]["releaseDate"]
+
+    print(">>>> Get latest price release for {}".format(ticker))
+    print("release date =", release_date)
+
+    data_points = content["data"]["data"][0]["dataPoints"]
+
+    for data_point in data_points:
+        period_start_at = data_point["deliveryPeriod"]["startAt"]
+        spark_price = data_point["derivedPrices"]["usdPerDay"]["spark"]
+
+        print(
+            "Spark Price is USD",
+            "{:>6}".format(spark_price),
+            "/day for period starting on",
+            period_start_at,
+        )
+
+
+def main(file_path=None):
+
+    print(">>>> Running Spark API Python sample...")
+
+    client_id, client_secret = retrieve_credentials(file_path)
+
+    # Authenticate:
+    access_token = get_access_token(client_id, client_secret)
+
+    # Fetch data:
+    tickers = list_contracts(access_token)
+    for ticker in tickers:
+        get_latest_price_releases(access_token, ticker)
+
+    print(">>>> Done!")
 
 
 if __name__ == "__main__":
-    main()
+
+    if len(sys.argv) >= 2:
+        main(file_path=sys.argv[1])
+    else:
+        main()
